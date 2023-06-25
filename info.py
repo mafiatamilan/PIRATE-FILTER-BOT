f24875497697a002a3240e4dc931087f889552c1
Bow ! Bow ! <105433790+mafiatamilan@users.noreply.github.com>
2023-06-25 10:36:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,10 +21,10 @@ BOT_TOKEN = environ['BOT_TOKEN']
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = is_enabled((environ.get('USE_CAPTION_FILTER', 'True')), True)
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/347218163657107544b48.jpg')).split()
-NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/347218163657107544b48.jpg")
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/8224a9f5f925826db99df.jpg https://te.legra.ph/file/068c7b2fbcfddbc1db13a.jpg')).split()
+NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/8224a9f5f925826db99df.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/451f038b4e7c2ddd10dc0.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/5e2d4418525832bc9a1b9.jpg")
+SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/4fed62431f1988168421b.jpg")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5179005671' ).split()]
